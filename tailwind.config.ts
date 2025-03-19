6f0c76f17efbc7dfa053b4afdc958518c38d99ec
Stephane Nouvellon <stephane@cloudflare.com>
2025-03-19 15:53:43 +0100

    update font family from sans to Roboto in globals.css and add Roboto to Tailwind CSS configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -69,6 +69,7 @@ const config = {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
         baba: ["BabaPro-Bold", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
       },
       keyframes: {
         "accordion-down": {
